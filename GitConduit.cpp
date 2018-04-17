899c6e6dd319b451f2751e4dec57cb4cdb6c8cf2
Crayon2000 <crayon1@rocketmail.com>
2018-04-17 09:27:24 -0400

    Put style in resource

--- GitConduit.cpp
+++ GitConduit.cpp
@@ -8,30 +8,44 @@
 //---------------------------------------------------------------------------
 USEFORM("Main.cpp", Form2);
 //---------------------------------------------------------------------------
+void __fastcall UpdateStyle();
+//---------------------------------------------------------------------------
 extern "C" int FMXmain()
 {
     try
     {
-         Application->Initialize();
-         Application->CreateForm(__classid(TForm2), &Form2);
+        UpdateStyle();
+        Application->Initialize();
+        Application->CreateForm(__classid(TForm2), &Form2);
          Application->Run();
     }
     catch (Exception &exception)
     {
-         Application->ShowException(&exception);
+        Application->ShowException(&exception);
     }
     catch (...)
     {
-         try
-         {
-             throw Exception("");
-         }
-         catch (Exception &exception)
-         {
-             Application->ShowException(&exception);
-         }
+        try
+        {
+            throw Exception("");
+        }
+        catch (Exception &exception)
+        {
+            Application->ShowException(&exception);
+        }
     }
     return 0;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall UpdateStyle()
+{
+    // If you call the SetStyle function in the initialization section of a unit on the main project file, before Application->Initialize, then it is applied to all forms.
+
+    // Set the style
+    Fmx::Types::TFmxObject* LStyle = TStyleStreaming::LoadFromResource(
+        (NativeUInt)HInstance, L"DATA_STYLE", RT_RCDATA);
+    TStyleManager::SetStyle(LStyle);
 }
 //---------------------------------------------------------------------------
 
